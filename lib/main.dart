ee3ecb1d98df550da5bef8166fa703e7248ff152
Elsayed87 <sayedbaraa2010@gmail.com>
2022-06-17 00:43:32 +0200

    blue color was v good for design

--- lib/main.dart
+++ lib/main.dart
@@ -108,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.title,color:Colors.yellow),
+        child: const Icon(Icons.title,color:Colors.blue),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
